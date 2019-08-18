8b2172b6c9ef7fa87ab7885eff6ce001425e6c2c
AplinXy9plin <evilberry@icloud.com>
2019-08-18 13:57:56 +0700

    Updates

---
+++ precache-manifest.6b4834fdc69c281278e98884a535aaab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d39cfc6b00832a939ea",
+    "url": "/ConversationCatalog-Frontend/static/js/runtime~main.48077d34.js"
+  },
+  {
+    "revision": "e60b8bc9441eb94d8a18",
+    "url": "/ConversationCatalog-Frontend/static/js/main.e25bb465.chunk.js"
+  },
+  {
+    "revision": "57aacd14a2ca5859eee1",
+    "url": "/ConversationCatalog-Frontend/static/js/2.72d7af17.chunk.js"
+  },
+  {
+    "revision": "57aacd14a2ca5859eee1",
+    "url": "/ConversationCatalog-Frontend/static/css/2.b08b948a.chunk.css"
+  },
+  {
+    "revision": "e3f1ac6a47011bbfda77e5cfee0e0368",
+    "url": "/ConversationCatalog-Frontend/index.html"
+  }
+];
